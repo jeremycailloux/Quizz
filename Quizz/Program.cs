27db8fb41506be59499b695f4e935c06d81a3155
Floups <fderosne@free.fr>
2019-12-23 09:32:45 +0100

    Creation du projet

---
+++ Quizz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
