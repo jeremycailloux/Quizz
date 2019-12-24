f5c1a933f7e8d4c3fbce8cdc300295735732a361
Marc Noris <mnoris.xeno@gmail.com>
2019-12-24 11:32:40 +0100

    Maj de 11h30

--- Quizz/Program.cs
+++ Quizz/Program.cs
@@ -6,16 +6,16 @@ namespace Quizz
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-           // Console.WriteLine("Saisissez votre Nom");
-           // string nom = Console.ReadLine();
-           // Console.WriteLine("Saisissez votre Prénom");
-           // string prenom = Console.ReadLine();
+            // Console.WriteLine("Saisissez votre Nom");
+            // string nom = Console.ReadLine();
+            // Console.WriteLine("Saisissez votre Prénom");
+            // string prenom = Console.ReadLine();
 
-           // VisualiserFichier();
-           //Console.ReadLine();
-           
+            // VisualiserFichier();
+            //Console.ReadLine();
+
             List<QCM> qcms = DAL.GetQCM();
 
 
@@ -29,38 +29,68 @@ namespace Quizz
             Console.ReadLine();
             Console.Clear();
 
-                //On initialise la variable 'compteur' de bonnes réponses à 0
-            int compteur=0;
+            //On initialise la variable 'compteur' de bonnes réponses à 0
+            int compteur = 0;
             for (int i = 0; i < qcms.Count; i++)
             {
-                
-            
-            //On affiche la question et ses propositions
-            Console.WriteLine(qcms[i]);
-            //on enregistre la chaine de caractères dans la variable réponse
-            string reponse=Console.ReadLine();
 
-            //si la réponse du joueur est correcte, on incrémente le compteur de 1
-            if (reponse==qcms[i].BonneRéponse)
-            {
-                compteur++;
-            }
+
+                //On affiche la question et ses propositions
+                Console.WriteLine(qcms[i]);
+                //on enregistre la chaine de caractères dans la variable réponse
+                string reponse = "";
+                bool repOK = false;
+
+                while (!repOK)
+                {
+                    try
+                    {
+                        reponse = Console.ReadLine();
+                        VerificationFormat(reponse);
+                        repOK = true;
+
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine(e.Message);
+
+                    }
+                }
+
+                //si la réponse du joueur est correcte, on incrémente le compteur de 1
+                if (reponse == qcms[i].BonneRéponse)
+                {
+                    compteur++;
+                }
 
             }
             Console.WriteLine("Votre score est de {0} sur {1}", compteur, qcms.Count);
 
         }
 
-
         
+        public static void VerificationFormat(string rep)
+        {  char valeur;
+            for (int i = 0; i < rep.Length; i++)
+            {
+                valeur = rep[i];
+                if (valeur > 64 && valeur < 91)
+                {
+                
+                }
+
+               else throw new FormatException("Le format de la réponse n'est pas valide, veuillez resaisir une réponse en lettres majuscules.");
+            }
+
+           
+        }
 
 
+        /* private static void VisualiserFichier()
+         {
+             string[] lignes = File.ReadAllLines(@"..\..\..\QCM.txt");
 
-       /* private static void VisualiserFichier()
-        {
-            string[] lignes = File.ReadAllLines(@"..\..\..\QCM.txt");
-                                                  
-        }*/
-        
+         }*/
+
     }
 }
