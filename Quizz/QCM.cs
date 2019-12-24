9f48a933d7aa8dccaf6315f70e0ecc0c62dd0e88
Marc Noris <mnoris.xeno@gmail.com>
2019-12-24 15:39:10 +0100

    maj Mr propre

--- Quizz/QCM.cs
+++ Quizz/QCM.cs
@@ -6,21 +6,15 @@ namespace Quizz
 {
     class QCM
     {
-
         private string _ligne;
         public string Question { get; set; } //Déclaration de la propriété Question
-
         public List<string> Propositions { get; set; } //Déclaration de la propriété Propositions qui est une liste
-
         public string BonneRéponse { get; set; } //Déclaration de la propriété BonneRéponse
-
-
         public QCM() //Création du constructeur QCM
         {
 
             Propositions = new List<string>();
         }
-
         /// <summary>
         /// Utilisation dela methode ToString de la classe Objet dont héritent toutes les classes. C'est pourquoi nous utilisons override dans son type pour faire appel à la classe ancêtre Objet.
         /// Elle permet d'afficher des chaînes de charactère, ici nous l'utilisons pour afficher les objet QCM.
@@ -40,7 +34,6 @@ namespace Quizz
 
             return Question + "\n" + prop;
         }
-
     }
 
     
